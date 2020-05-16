2b07b1e0aeb2d1854b9897bf466566b7e9607ba4
DesmondHung <alphon.hung@gmail.com>
2020-05-16 18:27:00 +0800

    v1 ok

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,33 @@
 <template>
   <div id="app">
+    <b-card header="Infinite Scroll" class="infinite-scroll-card text-center">
+      <p class="card-text">載入次數：{{ infiniteScrollCount }}</p>
+    </b-card>
     <div class="wrapper">
-      <b-modal ref="alert-modal" hide-footer>
-        <template v-slot:modal-title>通知訊息</template>
-        <div class="d-block text-center">
-          <h3>無法獲取Github Repos</h3>
-        </div>
-        <b-button class="mt-3" block @click="$refs['alert-modal'].hide()">關閉</b-button>
-      </b-modal>
-
-      <div class="repo-item" v-for="(repo, index) in repos" :key="index">
-        <div class="display-3">{{ repo.name }}</div>
-        <p>{{ repo.description }}</p>
-        <div class="bottom-row">
-          <div class="info-row">
-            <b-badge variant="dark">{{ repo.language }}</b-badge>
-            <b-badge>Created: {{ repo.created_at }}</b-badge>
-            <b-badge>Updated: {{ repo.updated_at }}</b-badge>
+      <h1>Github Repos with Infinite Scroll</h1>
+      <h6>Author: 洪士峰 Desmond&emsp;Date: 2020/05/16</h6>
+      <ul class="repos" ref="repoContainer">
+        <li class="repo-item" v-for="(repo, index) in repos" :key="index">
+          <div class="display-3">{{ repo.name }}</div>
+          <p>{{ repo.description }}</p>
+          <div class="bottom-row">
+            <div class="info-row">
+              <b-badge variant="info">{{ repo.language }}</b-badge>
+              <b-badge>Created: {{ repo.created_at }}</b-badge>
+              <b-badge>Updated: {{ repo.updated_at }}</b-badge>
+            </div>
+            <b-button :href="repo.html_url" target="_blank" variant="outline-info">Github Link</b-button>
           </div>
-          <b-button :href="repo.html_url" target="_blank">HTML Link</b-button>
-        </div>
-      </div>
-      <b-jumbotron class="repo-item" v-for="(repo, index) in repos" :key="index">
-        <template v-slot:header>{{ repo.name }}</template>
-        <template v-slot:lead>{{ repo.description }}</template>
-        <hr class="my-4" />
-        <p>測試</p>
-        <b-button variant="primary" :href="repo.html_url" target="_blank">Repo Link</b-button>
-      </b-jumbotron>
+        </li>
+      </ul>
     </div>
+    <b-modal ref="alert-modal" hide-footer>
+      <template v-slot:modal-title>通知訊息</template>
+      <div class="d-block text-center">
+        <h3>無法獲取Github Repos</h3>
+      </div>
+      <b-button class="mt-3" block @click="$refs['alert-modal'].hide()">關閉</b-button>
+    </b-modal>
   </div>
 </template>
 
@@ -37,32 +36,9 @@ export default {
   name: "app",
   data() {
     return {
-      repos: [
-        {
-          name: "123",
-          description: "haha",
-          html_url: "google.com",
-          created_at: "2020-05-14T17:04:47Z",
-          updated_at: "2020-05-15T17:11:32Z",
-          language: "JavaScript"
-        },
-        {
-          name: "456",
-          description: "haha2",
-          html_url: "google.com",
-          created_at: "2020-05-15T17:04:47Z",
-          updated_at: "2020-05-16T17:11:32Z",
-          language: "JavaScript"
-        },
-        {
-          name: "789",
-          description: "haha3",
-          html_url: "google.com",
-          created_at: "2020-05-16T17:04:47Z",
-          updated_at: "2020-05-17T17:11:32Z",
-          language: "JavaScript"
-        }
-      ]
+      infiniteScrollCount: 1,
+      allowAdd: true, // A flag for avoiding repeat request.
+      repos: []
     };
   },
   methods: {
@@ -74,22 +50,37 @@ export default {
         headers: {
           Authorization: "token e6ea03bc0020faf1cb8c884ffc6c26b47c1d0b0b"
         }
+        // params: { page: this.infiniteScrollCount, per_page: 100 }
       };
       this.axios.request(config).then(response => {
         if (!response.data) {
           this.$refs["alert-modal"].show();
-          // this.dismissCountDown = this.dismissSecs;
           return;
         }
-        this.repos = response.data;
-        console.log(this.repos);
+        this.repos.push.apply(this.repos, response.data);
+        this.allowAdd = true;
       });
+    },
+    setEvents() {
+      const that = this;
+      window.onscroll = function() {
+        if (!that.allowAdd) return;
+        if (
+          window.innerHeight + window.scrollY + 1 >=
+          document.body.offsetHeight
+        ) {
+          that.allowAdd = false;
+          that.infiniteScrollCount++;
+          that.getRepos();
+        }
+      };
     }
   },
   computed: {},
   created() {},
   mounted() {
-    // this.getRepos();
+    this.getRepos();
+    this.setEvents();
   },
   beforeDestroy() {}
 };
@@ -101,37 +92,45 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background: url("./assets/bg.jpg") repeat-x center top #f9f9f9;
+  background: url("./assets/bg.jpg") repeat-x center top fixed #f9f9f9;
   color: #2c3e50;
+  $color-border: #7caeba;
   @mixin flex-content($dir: row, $jus: center, $ali: center) {
     display: flex;
     flex-direction: $dir;
     justify-content: $jus;
     align-items: $ali;
   }
+  .infinite-scroll-card {
+    position: fixed;
+    right: 0;
+    top: 0;
+  }
   .wrapper {
     width: 1250px;
-    min-height: 100vh;
     margin: 0 auto;
-    padding: 1rem 0;
-    .repo-item {
-      @include flex-content(column, flex-start, center);
-      border: 2px solid #7caeba;
-      border-radius: 10px;
-      background: rgba(255, 255, 255, 0.8);
-      padding: 1rem 2rem;
-      &:nth-child(n + 2) {
-        margin-top: 1rem;
-      }
-      .bottom-row {
-        width: 100%;
-        @include flex-content(row, space-between, flex-end);
-      }
-      .info-row {
-        @include flex-content(row, flex-end, center);
-        .badge {
-          &:nth-child(n+2) {
-            margin-left: 1rem;
+    padding: 2rem 0;
+    .repos {
+      min-height: calc(100vh - 100px);
+      .repo-item {
+        @include flex-content(column, flex-start, center);
+        border: 2px solid $color-border;
+        border-radius: 10px;
+        background: rgba(255, 255, 255, 0.8);
+        padding: 2rem;
+        &:nth-child(n + 2) {
+          margin-top: 1rem;
+        }
+        .bottom-row {
+          width: 100%;
+          @include flex-content(row, space-between, flex-end);
+        }
+        .info-row {
+          @include flex-content(row, flex-end, center);
+          .badge {
+            &:nth-child(n + 2) {
+              margin-left: 1rem;
+            }
           }
         }
       }
